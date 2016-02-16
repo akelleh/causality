ed584eed4b5b72af15c7a5db3233a0bb6af573d0
Adam Kelleher <adam.kelleher@buzzfeed.com>
2016-02-16 18:38:35 -0500

    small changes

--- causality/inference/independence_tests/__init__.py
+++ causality/inference/independence_tests/__init__.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import statsmodels.api as sm
 
 
@@ -23,4 +24,38 @@ class RobustRegressionTest():
                 return True
         
         
-    
+class DiscreteLogisticTest():
+    def __init__(self, y, x, z, data, alpha):
+        self.dummy_df = pd.get_dummies(data[x[0])
+        independent_dummies = []
+        for col in (x[1:] + z):
+            dummies = pd.get_dummies(data[col], prefix=col)
+            dummies_to_keep = dummies.columns[1:]
+            independent_dummies += dummies_to_keep
+            self.dummy_df[dummies_to_keep] = dummies[dummies_to_keep]
+        dependent_dummies = []
+        for col in y:
+            dummies = pd.get_dummies(data[col], prefix=col)
+            dummies_to_keep = dummies.columns[1:]
+            dependent_dummies += dummies_to_keep
+            self.dummy_df[dummies_to_keep] = dummies[dummies_to_keep] 
+        self.dummy_df['intercept'] = 1.
+        self.regression = sm.Logit(data[dependent_dummies], data[independent_dummies] ) 
+        self.result = self.regression.fit()
+        self.coefficient = self.result.params[x][0]
+        confidence_interval = self.result.conf_int(alpha=alpha/2.)
+        self.upper = confidence_interval[1][x][0]
+        self.lower = confidence_interval[0][x][0]
+        
+
+    def independent(self):
+        if self.coefficient > 0.:
+            if self.lower > 0.:
+                return False
+            else:
+                return True
+        else:
+            if self.upper < 0.:
+                return False
+            else:
+                return True 
