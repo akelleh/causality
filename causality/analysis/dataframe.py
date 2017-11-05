ef9387dc4b07d693a43a140977d7e7cc6e38e215
Adam Kelleher <akelleh@gmail.com>
2017-11-05 15:00:38 -0500

    changed to kernel regression

--- causality/analysis/dataframe.py
+++ causality/analysis/dataframe.py
@@ -1,21 +1,27 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
+from statsmodels.nonparametric.kernel_regression import KernelReg
 
 class CausalDataFrame(pd.DataFrame):
     def zplot(self, *args, **kwargs):
-        if kwargs.get('z', []):
+        if kwargs.get('z', {}):
             if kwargs.get('kind') == 'line':
                 treatment = kwargs.get('x')
                 outcome = kwargs.get('y')
-                confounders = kwargs.get('z', [])
+                variable_types = kwargs.get('z', {})
+                variable_types[treatment] = 'c'
+                confounders = kwargs.get('z', {}).keys()
 
-                model = RandomForestRegressor(n_estimators=1000, n_jobs=8)
-                model.fit(self[[treatment] + confounders], self[outcome])
+                if kwargs.get('model'):
+                    model = kwargs.get('model')()
+                    model.fit(self[[treatment] + confounders], self[outcome])
+                else:
+                    model = KernelModelWrapper()
+                    model.fit(self[[treatment] + confounders], self[outcome], variable_types=variable_types)
 
                 xs = []
                 ys = []
-                xmin, xmax = kwargs.get('xlim', (-1,1))
+                xmin, xmax = kwargs.get('xlim', (self[treatment].quantile(0.01), self[treatment].quantile(0.99)))
                 for xi in np.arange(xmin, xmax, (xmax - xmin) / 100.):
                     df = self.copy()
                     df[treatment] = xi
@@ -29,3 +35,25 @@ class CausalDataFrame(pd.DataFrame):
 
         else:
             self.plot(*args, **kwargs)
+
+class KernelModelWrapper(object):
+    def __init__(self):
+        self.model = None
+        self.variable_types = {}
+        self.X_shape = None
+        self.y_shape = None
+
+    def fit(self, X, y, variable_types={}):
+        self.X_shape = X.shape
+        self.y_shape = y.shape
+        if variable_types:
+            variable_type_string = ''.join([variable_types[col] for col in X.columns])
+            self.model = KernelReg(y, X, variable_type_string, reg_type='ll')
+        else:
+            self.model = KernelReg(y, X, 'c' * X.shape[1], reg_type='ll')
+        return self
+
+    def predict(self, X):
+        if X.shape != self.X_shape:
+            raise Exception("Expected shape {}, received {}".format(self.X_shape, X.shape))
+        return self.model.fit(X)[0]
